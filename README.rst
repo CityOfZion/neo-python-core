f562ecd4d96cd650df09a6afb1938cc8be546166
Chris Hager <chris@linuxuser.at>
2017-12-30 11:21:36 +0100

    intitial docs, cleanup and setup

--- README.rst
+++ README.rst
@@ -2,13 +2,35 @@
 neo-python-core
 ===============
 
-Core functionality of neo-python
-
-* Free software: MIT license
+Core functionality of [neo-python](https://github.com/CityOfZion/neo-python).
 
 
-Contributors
-------------
+## Getting started
 
-* localhuman
-* metachris
+You need [Python 3.5](https://www.python.org/downloads/release/python-354/).
+
+
+### Virtual Environment
+
+Create a Python 3 virtual environment and activate it:
+
+.. code-block:: console
+
+    $ python3 -m venv venv
+    $ source venv/bin/activate
+
+Then install the requirements:
+
+.. code-block:: console
+
+    $ pip install -e .
+    $ pip install -r requirements_dev.txt
+
+### Linting and testing the code
+
+.. code-block:: console
+
+    make lint
+    make test
+    make test-all
+    make coverage
--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+from pip.download import PipSession
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -11,46 +13,31 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    # TODO: put package requirements here
-]
-
-setup_requirements = [
-    # TODO(metachris): put setup requirements (distutils extensions, etc.) here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
-]
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='neo-python-core',
+    python_requires='>=3.4, <3.6',
     version='0.1.0',
     description="Core functionality of neo-python",
     long_description=readme + '\n\n' + history,
     author="City of Zion",
-    author_email='chris@cityofzion.io',
+    author_email='python@cityofzion.io',
     url='https://github.com/CityOfZion/neo-python-core',
     packages=find_packages(include=['neocore']),
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=reqs,
     license="MIT license",
     zip_safe=False,
-    keywords='neo-python-core',
+    keywords='neo-python-core neo, python, node',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
+    ]
 )
