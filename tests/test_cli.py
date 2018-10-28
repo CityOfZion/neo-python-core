d847285ddecfed266ca9e671f734d1998b4939c3
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-10-28 02:49:24 -0700

    Update test_cli.py (#56)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-
 from neocore.bin import cli
+import subprocess
+import warnings
+import neocore
 
 
 class CliTestCase(TestCase):
@@ -32,3 +34,24 @@ class CliTestCase(TestCase):
         self.assertIn("address", wallet)
         self.assertIsInstance(wallet["private_key"], str)
         self.assertIsInstance(wallet["address"], str)
+
+    def test_main(self):
+        warnings.filterwarnings('ignore', category=ResourceWarning)  # filters warnings about subprocess files still being open
+
+        version = subprocess.Popen(['np-utils', '--version'], stdout=subprocess.PIPE)
+        self.assertIn(neocore.__version__, version.stdout.read().decode('utf-8'))
+
+        address_to_scripthash = subprocess.Popen(['np-utils', '--address-to-scripthash', 'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y'], stdout=subprocess.PIPE)
+        self.assertEqual(b'Scripthash big endian:  0xe9eed8dc39332032dc22e5d6e86332c50327ba23\nScripthash little endian: 23ba2703c53263e8d6e522dc32203339dcd8eee9\nScripthash neo-python format: b\'#\\xba\\\'\\x03\\xc52c\\xe8\\xd6\\xe5"\\xdc2 39\\xdc\\xd8\\xee\\xe9\'\n', address_to_scripthash.stdout.read())
+
+        scripthash_to_address = subprocess.Popen(['np-utils', '--scripthash-to-address', "0xe9eed8dc39332032dc22e5d6e86332c50327ba23"], stdout=subprocess.PIPE)
+        self.assertIn("AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y", scripthash_to_address.stdout.read().decode('utf-8'))
+
+        wallet1 = subprocess.Popen(['np-utils', '--create-wallet'], stdout=subprocess.PIPE)
+        self.assertIn("private_key", wallet1.stdout.read().decode('utf-8'))
+
+        wallet2 = subprocess.Popen(['np-utils', '--create-wallet'], stdout=subprocess.PIPE)
+        self.assertIn("address", wallet2.stdout.read().decode('utf-8'))
+
+        print_help = subprocess.Popen(['np-utils', '--h'], stdout=subprocess.PIPE)
+        self.assertIn("help", print_help.stdout.read().decode('utf-8'))
